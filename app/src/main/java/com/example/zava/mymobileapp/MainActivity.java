60d9391ddd63a166d3f03478bdacdd8b9ef4ed77
Nazarko Zavarynskyi <nazarko1805@gmail.com>
2018-09-10 14:16:33 +0300

    [+] lab2

--- app/src/main/java/com/example/zava/mymobileapp/MainActivity.java
+++ app/src/main/java/com/example/zava/mymobileapp/MainActivity.java
@@ -2,13 +2,48 @@ package com.example.zava.mymobileapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    public EditText etName;
+    TextView textWithName;
+    Button clearText, sayHello;
+    public String name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("Hello world :)"); //test output to console
+        System.out.println("Hello World :)");
+
+        etName = findViewById(R.id.etName);
+        textWithName = findViewById(R.id.textWithName);
+        clearText = findViewById(R.id.clearText);
+        sayHello = findViewById(R.id.sayHello);
+
+        sayHello.setOnClickListener(this);
+        clearText.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) { // switch to conrol clicks between two different buttons
+            case R.id.clearText:
+                etName.getText().clear();
+                break;
+            case R.id.sayHello:
+                textWithName.setText("Hello, " + etName.getText().toString());// output our congratulation
+                etName.getText().clear();
+        }
+    }
+
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
     }
 }
