78f901ec2d043aa801a0b911319ca2043ddfc673
Unknown <nazarko1805@gmail.com>
2018-10-11 17:14:26 +0300

    [^] ffixes

--- app/src/main/java/com/example/zava/mymobileapp/adapters/CardAdapter.java
+++ app/src/main/java/com/example/zava/mymobileapp/adapters/CardAdapter.java
@@ -17,80 +17,81 @@ import java.util.List;
 import com.example.zava.mymobileapp.model.CardAlbum;
 import com.example.zava.mymobileapp.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
-public class CardAdapter extends RecyclerView.Adapter<CardAdapter.CardViewHolder>{
 
-    private List<CardAlbum> cardAlbumList;
-    private LayoutInflater layoutInflater;
-    private CardClickCallBack cardClickCallBack;
-    private Context context;
+public class CardAdapter extends RecyclerView.Adapter<CardAdapter.CardViewHolder> {
 
-    public CardAdapter(List<CardAlbum> cardAlbumList, Context context) {
-        this.cardAlbumList = cardAlbumList;
-        this.context = context;
-        this.layoutInflater = layoutInflater.from(context);
+  private List<CardAlbum> cardAlbumList;
+  private LayoutInflater layoutInflater;
+  private CardClickCallBack cardClickCallBack;
+  private Context context;
+
+  public CardAdapter(List<CardAlbum> cardAlbumList, Context context) {
+    this.cardAlbumList = cardAlbumList;
+    this.context = context;
+    this.layoutInflater = layoutInflater.from(context);
+  }
+
+  public interface CardClickCallBack {
+    public void onCardClick(int position);
+
+    public void onCardButtonClick(int position);
+  }
+
+  public void setCardClickCallBack(CardClickCallBack cardClickCallBack) {
+    this.cardClickCallBack = cardClickCallBack;
+  }
+
+  @Override
+  public CardViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    View view = layoutInflater.inflate(R.layout.card_item, parent, false);
+    return new CardViewHolder(view);
+  }
+
+  @Override
+  public void onBindViewHolder(CardViewHolder holder, int position) {
+    CardAlbum cardAlbum = cardAlbumList.get(position);
+    Picasso.with(context).setLoggingEnabled(true);
+    Picasso.with(context).load(cardAlbum.getAlbumImageURL()).into(holder.imageView_Avatar);
+    holder.textView_albumName.setText(cardAlbum.getAlbumName());
+    holder.textView_artistName.setText(cardAlbum.getArtistName());
+    holder.button_aboutArtist.setText("About " + cardAlbum.getArtistName());
+  }
+
+  @Override
+  public int getItemCount() {
+    return cardAlbumList.size();
+  }
+
+
+  class CardViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+
+    private View viewContainer;
+    @BindView(R.id.imageView_for_album_photo) ImageView imageView_Avatar;
+    @BindView(R.id.textView_for_album_name) TextView textView_albumName;
+    @BindView(R.id.textView_for_artist_names) TextView textView_artistName;
+    @BindView(R.id.button_for_about_artist) Button button_aboutArtist;
+
+
+    public CardViewHolder(View itemView) {
+      super(itemView);
+      ButterKnife.bind(this,itemView);
+      button_aboutArtist.setOnClickListener(this);
+      viewContainer = itemView.findViewById(R.id.cardview_element);
+      viewContainer.setOnClickListener(this);
     }
 
-    public interface CardClickCallBack{
-        public void onCardClick(int position);
-        public void onCardButtonClick(int position);
-    }
-
-    public void setCardClickCallBack(CardClickCallBack cardClickCallBack) {
-        this.cardClickCallBack = cardClickCallBack;
-    }
 
     @Override
-    public CardViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view = layoutInflater.inflate(R.layout.card_item,parent,false);
-        return new CardViewHolder(view);
+    public void onClick(View view) {
+      if (view.getId() == R.id.button_for_about_artist) {
+        cardClickCallBack.onCardButtonClick(getAdapterPosition());
+      } else {
+        cardClickCallBack.onCardClick(getAdapterPosition());
+      }
     }
-
-    @Override
-    public void onBindViewHolder(CardViewHolder holder, int position) {
-        CardAlbum cardAlbum = cardAlbumList.get(position);
-        Picasso.with(context).setLoggingEnabled(true);
-        Picasso.with(context).load(cardAlbum.getAlbumImageURL()).into(holder.imageView_Avatar);
-        holder.textView_albumName.setText(cardAlbum.getAlbumName());
-        holder.textView_artistName.setText(cardAlbum.getArtistName());
-        holder.button_aboutArtist.setText("About "+cardAlbum.getArtistName());
-    }
-
-    @Override
-    public int getItemCount() {
-        return cardAlbumList.size();
-    }
-
-
-    class CardViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
-
-        private ImageView imageView_Avatar;
-        private TextView textView_albumName;
-        private TextView textView_artistName;
-        private Button button_aboutArtist;
-        private View viewContainer;
-
-
-        public CardViewHolder(View itemView) {
-            super(itemView);
-
-            imageView_Avatar = (ImageView)itemView.findViewById(R.id.imageView_for_album_photo);
-            textView_albumName = (TextView)itemView.findViewById(R.id.textView_for_album_name);
-            textView_artistName = (TextView)itemView.findViewById(R.id.textView_for_artist_names);
-            button_aboutArtist = (Button)itemView.findViewById(R.id.button_for_about_artist);
-            button_aboutArtist.setOnClickListener(this);
-            viewContainer = itemView.findViewById(R.id.cardview_element);
-            viewContainer.setOnClickListener(this);
-        }
-
-
-        @Override
-        public void onClick(View view) {
-            if(view.getId() == R.id.button_for_about_artist){
-                cardClickCallBack.onCardButtonClick(getAdapterPosition());
-            }else{
-                cardClickCallBack.onCardClick(getAdapterPosition());
-            }
-        }
-    }
+  }
 }
---
+++ app/src/main/java/com/example/zava/mymobileapp/model/CardAlbum.java
@@ -0,0 +1,87 @@
+package com.example.zava.mymobileapp.model;
+
+import java.util.List;
+
+public class CardAlbum {
+
+    private String albumId;
+    private String albumName;
+    private String artistId;
+    private String artistName;
+    private String albumImageURL;
+    private int albumPopularity;
+    private String albumReleaseDate;
+
+    /**
+     * Constructor for the instance variables.
+     * @param albumId
+     * @param albumImageURL
+     * @param albumName
+     * @param artistName
+     */
+    public CardAlbum(String albumId, String albumName, String artistId, String artistName, String albumImageURL, int albumPopularity, String albumReleaseDate) {
+        this.albumId = albumId;
+        this.albumName = albumName;
+        this.artistId = artistId;
+        this.artistName = artistName;
+        this.albumImageURL = albumImageURL;
+        this.albumPopularity = albumPopularity;
+        this.albumReleaseDate = albumReleaseDate;
+    }
+
+    public String getAlbumId() {
+        return albumId;
+    }
+
+    public void setAlbumId(String albumId) {
+        this.albumId = albumId;
+    }
+
+    public String getAlbumImageURL() {
+        return albumImageURL;
+    }
+
+    public void setAlbumImageURL(String albumImageURL) {
+        this.albumImageURL = albumImageURL;
+    }
+
+    public String getArtistId() {
+        return artistId;
+    }
+
+    public void setArtistId(String artistId) {
+        this.artistId = artistId;
+    }
+
+    public String getAlbumName() {
+        return albumName;
+    }
+
+    public void setAlbumName(String albumName) {
+        this.albumName = albumName;
+    }
+
+    public String getArtistName() {
+        return artistName;
+    }
+
+    public void setArtistName(String artistName) {
+        this.artistName = artistName;
+    }
+
+    public int getAlbumPopularity() {
+        return albumPopularity;
+    }
+
+    public void setAlbumPopularity(int albumPopularity) {
+        this.albumPopularity = albumPopularity;
+    }
+
+    public String getAlbumReleaseDate() {
+        return albumReleaseDate;
+    }
+
+    public void setAlbumReleaseDate(String albumReleaseDate) {
+        this.albumReleaseDate = albumReleaseDate;
+    }
+}
