c4b8022ea5ec47db1372192f402925fcbcea94d1
Unknown <nazarko1805@gmail.com>
2018-11-19 22:54:39 +0200

    [^] fix

--- LAB8/app/src/main/java/com/zava/mvplab/data/api/retrofit/deserializer/ListDeserializer.java
+++
@@ -1,8 +0,0 @@
-package com.zava.mvplab.data.api.retrofit.deserializer;
-
-import com.google.gson.JsonDeserializer;
-
-import java.util.List;
-
-interface ListDeserializer<T> extends JsonDeserializer<List<T>> {
-}
